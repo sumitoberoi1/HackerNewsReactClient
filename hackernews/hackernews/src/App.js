a56c0d828dc47b5eea4f0eb54475cb4a5afc6925
Sumit Oberoi <sumit.oberoi1@gmail.com>
2018-09-26 17:19:25 -0400

    Searchbar and filter added

--- hackernews/hackernews/src/App.js
+++ hackernews/hackernews/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isString } from 'util';
 const list = [
   {
     title: 'React',
@@ -21,19 +22,39 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm : ''
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onDismiss(objectID) {
+    console.log(this);
+    const modifiedList = this.state.list.filter(item=>item.objectID !== objectID);
+    this.setState({list:modifiedList});
+  }
+  onSearchChange(event) {
+    this.setState({searchTerm:event.target.value});
+  }
+
   render() {
     const hello = ` 1 road to learning react with var`;
     return (
       <div className = "App">
+      <form>
+        <input type="text" onChange = {this.onSearchChange}/>
+      </form>
         {
-          list.map(item => 
+          this.state.list.filter(item => item.title.toLowerCase().includes(this.state.searchTerm.toLocaleLowerCase())).map(item => 
             <div key = {item.objectID}>
               <span>
                 <a href = {item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.points}</span>
-              {item.title}
+              <span> <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button></span>
             </div>)
         }
         {
