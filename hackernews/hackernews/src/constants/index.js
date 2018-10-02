d75581a3151f3dbc3c47be7a78cd2a56a521ddcc
Sumit Oberoi <sumit.oberoi1@gmail.com>
2018-10-02 02:09:04 -0400

    Directory structure changed

--- hackernews/hackernews/src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_QUERY = 'redux';
-export const PATH_BASE = 'https://hn.algolia.com/api/v1';
-export const PATH_SEARCH = '/search';
-export const PARAM_SEARCH = 'query=';
-export const PARAM_PAGE = 'page=';
-export const DEFAULT_HPP = '100';
-export const PARAM_HPP = 'hitsPerPage=';
